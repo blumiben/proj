2db30571f7515fcfb7105ffab10a8f99aa817377
blumiben <86224737+blumiben@users.noreply.github.com>
2022-05-04 14:41:56 +0300

    d

---
+++ src/environments/app/models/socks_sales.ts
@@ -0,0 +1,16 @@
+
+
+export class Sales {
+    public date!: string;
+    sales!:number;
+    orders!:number;
+    items!: number;
+    customers!: number
+    avarage:number
+   }
+   
+export class soks_sales {
+ public name!: string;
+   public month!: string;
+   public sales!:Sales
+}
